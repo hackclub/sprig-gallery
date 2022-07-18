d1d1b30c3cbc6bae9215c18a485d323b6825a135
leomcelroy <leomcelroy@gmail.com>
2022-07-18 15:30:44 -0400

    added getting started tutorial

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -121,7 +121,7 @@
 				<a href="https://sprig.hackclub.dev" class="button big-button">
 					Create Your Own
 				</a>
-				<a href="/getting-started" class="button big-button">Getting Started</a>
+				<a href="https://github.com/hackclub/sprig-gallery/blob/main/src/routes/getting-started.md#lets-make-our-first-game-in-sprig" class="button big-button">Getting Started</a>
 				<a href="/share" class="button big-button">Share your game</a>
 			</div>
 		</div>
