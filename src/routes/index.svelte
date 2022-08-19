7fcda033e1aded569019729913622d05de2c0da3
Kognise <felix.mattick@gmail.com>
2022-08-19 17:21:41 -0400

    YELLOW!!!

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -70,82 +70,88 @@
   let activeFilter = null;
 </script>
 
-<div bind:this={preloader} id="preloader">
-  <img src="/loader.gif" alt="coin preloader" />
-  <p>loading...</p>
-</div>
+<body>
+  <div bind:this={preloader} id="preloader">
+    <img src="/loader.gif" alt="coin preloader" />
+    <p>loading...</p>
+  </div>
 
-<div class="wrapper">
-  <div class="info-outer">
-    <div class="info-inner">
-      <div class="gallery-title">
-        <img class="gallery-header" src="/gallery.svg" alt="gallery header" draggable="false" />
-        <p>
-          The best way to learn is by making things that you care about and sharing them with other people.
-          <br /><br />
-          Check out games by other Hack Clubbers! Click to play each game and to hack on the code.
-        </p>
+  <div class="wrapper">
+    <div class="info-outer">
+      <div class="info-inner">
+        <div class="gallery-title">
+          <img class="gallery-header" src="/gallery.svg" alt="gallery header" draggable="false" />
+          <p>
+            The best way to learn is by making things that you care about and sharing them with other people.
+            <br /><br />
+            Check out games by other Hack Clubbers! Click to play each game and to hack on the code.
+          </p>
+        </div>
+
+        <div class="tag-container">
+          <fieldset>
+            <legend>Filter By Tag</legend>
+            {#each tags as tag}
+              <button
+                id={tag}
+                class={activeFilter === tag ? 'btn-tag btn active' : 'btn-tag btn'}
+                on:click={() => (activeFilter = activeFilter === tag ? null : tag)}
+              >
+                {tag}
+              </button>
+            {/each}
+          </fieldset>
+        </div>
+
+        <div class="btn-container">
+          <p>Want to join in on the fun? If you have a Sprig game to share with the community, click here!</p>
+          <a href="/share">
+            <button class="btn active">Add Your Game</button>
+          </a>
+        </div>
       </div>
+    </div>
+    <a class="logo" href="https://hackclub.com"
+      ><img src="https://assets.hackclub.com/flag-orpheus-top.svg" alt="hack club logo" /></a
+    >
+    <a href="https://github.com/hackclub/sprig/tree/main/games" target="_blank"
+      ><img src="./spriglogotext.png" alt="sprig logo" class="sprig-logo" /></a
+    >
+    <img src="/SPRIGDINO.svg" alt="sprig dino" class="sprig-dino" />
+    <div class="gallery-outer">
+      <div class="gallery-inner" bind:this={gallery}>
+        <Card id="start-from-scratch" />
 
-      <div class="tag-container">
-        <fieldset>
-          <legend>Filter By Tag</legend>
-          {#each tags as tag}
-            <button
-              id={tag}
-              class={activeFilter === tag ? 'btn-tag btn active' : 'btn-tag btn'}
-              on:click={() => (activeFilter = activeFilter === tag ? null : tag)}
-            >
-              {tag}
-            </button>
-          {/each}
-        </fieldset>
-      </div>
+        {#each games as game}
+          <!-- Tutorials first, or whatever the filter is -->
+          {#if game.tags.includes(activeFilter || 'tutorial')}
+            <Card name={game.name} imgURL={game.imgURL} tags={game.tags} author={game.author} />
+          {/if}
+        {/each}
 
-      <div class="btn-container">
-        <p>Want to join in on the fun? If you have a Sprig game to share with the community, click here!</p>
-        <a href="/share">
-          <button class="btn active">Add Your Game</button>
-        </a>
+        {#each games as game}
+          <!-- Everything but tutorials, or nothing if we're filtering -->
+          {#if !game.tags.includes('tutorial') && !activeFilter}
+            <Card name={game.name} imgURL={game.imgURL} tags={game.tags} author={game.author} />
+          {/if}
+        {/each}
       </div>
     </div>
   </div>
-  <a class="logo" href="https://hackclub.com"
-    ><img src="https://assets.hackclub.com/flag-orpheus-top.svg" alt="hack club logo" /></a
-  >
-  <a href="https://github.com/hackclub/sprig/tree/main/games" target="_blank"
-    ><img src="./spriglogotext.png" alt="sprig logo" class="sprig-logo" /></a
-  >
-  <img src="/SPRIGDINO.svg" alt="sprig dino" class="sprig-dino" />
-  <div class="gallery-outer">
-    <div class="gallery-inner" bind:this={gallery}>
-      <Card id="start-from-scratch" />
-
-      {#each games as game}
-        <!-- Tutorials first, or whatever the filter is -->
-        {#if game.tags.includes(activeFilter || 'tutorial')}
-          <Card name={game.name} imgURL={game.imgURL} tags={game.tags} author={game.author} />
-        {/if}
-      {/each}
-
-      {#each games as game}
-        <!-- Everything but tutorials, or nothing if we're filtering -->
-        {#if !game.tags.includes('tutorial') && !activeFilter}
-          <Card name={game.name} imgURL={game.imgURL} tags={game.tags} author={game.author} />
-        {/if}
-      {/each}
-    </div>
-  </div>
-</div>
+</body>
 
 <style lang="scss">
   @import '../styles/_variables.scss';
   @import '../styles/_fonts.scss';
   @import '../styles/_shared.scss';
 
+  body {
+    background: $base-background;
+    color: white;
+  }
+
   .wrapper {
-    background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('/pixelart_ruins_girl_trees.png'),
-      $base-background;
+    background: url('/background.jpg'), $base-background;
     background-size: cover;
     background-attachment: fixed;
     background-position: center center;
--- src/routes/share.svelte
+++ src/routes/share.svelte
@@ -1,141 +1,148 @@
-<div class="wrapper">
-  <a class="logo" href="https://hackclub.com">
-    <img src="https://assets.hackclub.com/flag-orpheus-top.svg" alt="hack club logo" />
-  </a>
+<body>
+  <div class="wrapper">
+    <a class="logo" href="https://hackclub.com">
+      <img src="https://assets.hackclub.com/flag-orpheus-top.svg" alt="hack club logo" />
+    </a>
 
-  <div class="heading-outer">
-    <div class="heading-inner">
-      <h1>Share your game</h1>
-      <p>
-        After you <a href="https://sprig.hackclub.dev/" target="_blank">create your own game</a>, we'd love for you to
-        <a href="/">share it with other makers</a> on the internet!
-      </p>
-    </div>
-  </div>
-
-  <div class="inner">
-    <div class="step">
-      <div>
-        <p class="step-number">Step 0:</p>
-        <h2>Name your game</h2>
-        <p>Add the following comment.</p>
-      </div>
-      <code class="media">
-        /*<br />
-        @title: your_game<br />
-        @author: your name<br />
-        */
-      </code>
-    </div>
-
-    <div class="step">
-      <div>
-        <p class="step-number">Step 1:</p>
-        <h2>Download your game file</h2>
-        <p>Select "share" then "as file". Your download should begin immediately.</p>
-      </div>
-      <img
-        class="media"
-        src="https://cloud-hemofvnr5-hack-club-bot.vercel.app/0download.png"
-        alt="screenshot of where to export game file"
-      />
-    </div>
-
-    <div class="step">
-      <div>
-        <p class="step-number">Step 2:</p>
-        <h2>Fork the Sprig repository</h2>
-        <p>Click below to create a fork of the Sprig repository.</p>
-        <p>You might have to sign up for a GitHub account.</p>
-        <a href="https://github.com/hackclub/sprig/fork" target="_blank">
-          <button class="btn active">Fork hackclub/sprig</button>
-        </a>
-      </div>
-      <img
-        class="media"
-        src="https://cloud-jps0pn7d1-hack-club-bot.vercel.app/0screenshot_2022-07-07_at_11.40.19_pm.png"
-        alt="screenshot of where to fork repository"
-      />
-    </div>
-
-    <div class="step">
-      <div>
-        <p class="step-number">Step 3:</p>
-        <h2>Upload your game to your fork</h2>
-        <ul>
-          <li>Open the "Games" folder within your fork</li>
-          <li>Click "Add File"</li>
-          <li>Select "Upload File"</li>
-          <li>Add your file and click "Commit File"</li>
-        </ul>
-      </div>
-      <img
-        class="media"
-        src="https://cloud-a7l5tmw39-hack-club-bot.vercel.app/0screenshot_2022-07-07_at_11.51.32_pm.png"
-        alt="screenshot of where to upload game file option is"
-      />
-    </div>
-
-    <div class="step">
-      <div>
-        <p class="step-number">Step 4:</p>
-        <h2>Create a pull request</h2>
-        <ul>
-          <li>On your fork's page, click on "Contribute"</li>
-          <li>Select "Open Pull Request"</li>
-          <li>Click "Create Pull Request"</li>
-          <li>Name your pull request with the name of your game</li>
-          <li>Click "Create Pull Request"</li>
-        </ul>
-        <p>We'll take a look at your game! Once approved, your game will appear in the gallery :)</p>
-      </div>
-      <img
-        class="media"
-        src="https://cloud-af1r71tvz-hack-club-bot.vercel.app/0screenshot_2022-07-07_at_11.52.55_pm.png"
-        alt="screenshot of pull request page"
-      />
-    </div>
-
-    <div class="step">
-      <div>
-        <p class="step-number">Optional:</p>
-        <h2>Add custom thumbnail</h2>
+    <div class="heading-outer">
+      <div class="heading-inner">
+        <h1>Share your game</h1>
         <p>
-          By default, the thumbnail that shows up in the gallery will be the first map of your game. If you want to
-          change this, you can:
+          After you <a href="https://sprig.hackclub.dev/" target="_blank">create your own game</a>, we'd love for you to
+          <a href="/">share it with other makers</a> on the internet!
         </p>
-        <ul>
-          <li>Take a screenshot of your game</li>
-          <li>Click back to the gallery folder</li>
-          <li>Open the img folder in gallery</li>
-          <li>Click "Add File"</li>
-          <li>Select "Upload File"</li>
-          <li>Name your image file the file name for your game</li>
-          <li>Click "Commit File"</li>
-        </ul>
       </div>
-      <img
-        class="media"
-        src="https://user-images.githubusercontent.com/27078897/184410129-a2dbf444-ae7e-442b-b4bd-d3f21ede54cc.png"
-        alt="screenshot of pull request page"
-      />
     </div>
 
-    <a href="/" class="bottom-cta"><button class="btn active">Check out the gallery &raquo;</button></a>
+    <div class="inner">
+      <div class="step">
+        <div>
+          <p class="step-number">Step 0:</p>
+          <h2>Name your game</h2>
+          <p>Add the following comment.</p>
+        </div>
+        <code class="media">
+          /*<br />
+          @title: your_game<br />
+          @author: your name<br />
+          */
+        </code>
+      </div>
+
+      <div class="step">
+        <div>
+          <p class="step-number">Step 1:</p>
+          <h2>Download your game file</h2>
+          <p>Select "share" then "as file". Your download should begin immediately.</p>
+        </div>
+        <img
+          class="media"
+          src="https://cloud-hemofvnr5-hack-club-bot.vercel.app/0download.png"
+          alt="screenshot of where to export game file"
+        />
+      </div>
+
+      <div class="step">
+        <div>
+          <p class="step-number">Step 2:</p>
+          <h2>Fork the Sprig repository</h2>
+          <p>Click below to create a fork of the Sprig repository.</p>
+          <p>You might have to sign up for a GitHub account.</p>
+          <a href="https://github.com/hackclub/sprig/fork" target="_blank">
+            <button class="btn active">Fork hackclub/sprig</button>
+          </a>
+        </div>
+        <img
+          class="media"
+          src="https://cloud-jps0pn7d1-hack-club-bot.vercel.app/0screenshot_2022-07-07_at_11.40.19_pm.png"
+          alt="screenshot of where to fork repository"
+        />
+      </div>
+
+      <div class="step">
+        <div>
+          <p class="step-number">Step 3:</p>
+          <h2>Upload your game to your fork</h2>
+          <ul>
+            <li>Open the "Games" folder within your fork</li>
+            <li>Click "Add File"</li>
+            <li>Select "Upload File"</li>
+            <li>Add your file and click "Commit File"</li>
+          </ul>
+        </div>
+        <img
+          class="media"
+          src="https://cloud-a7l5tmw39-hack-club-bot.vercel.app/0screenshot_2022-07-07_at_11.51.32_pm.png"
+          alt="screenshot of where to upload game file option is"
+        />
+      </div>
+
+      <div class="step">
+        <div>
+          <p class="step-number">Step 4:</p>
+          <h2>Create a pull request</h2>
+          <ul>
+            <li>On your fork's page, click on "Contribute"</li>
+            <li>Select "Open Pull Request"</li>
+            <li>Click "Create Pull Request"</li>
+            <li>Name your pull request with the name of your game</li>
+            <li>Click "Create Pull Request"</li>
+          </ul>
+          <p>We'll take a look at your game! Once approved, your game will appear in the gallery :)</p>
+        </div>
+        <img
+          class="media"
+          src="https://cloud-af1r71tvz-hack-club-bot.vercel.app/0screenshot_2022-07-07_at_11.52.55_pm.png"
+          alt="screenshot of pull request page"
+        />
+      </div>
+
+      <div class="step">
+        <div>
+          <p class="step-number">Optional:</p>
+          <h2>Add custom thumbnail</h2>
+          <p>
+            By default, the thumbnail that shows up in the gallery will be the first map of your game. If you want to
+            change this, you can:
+          </p>
+          <ul>
+            <li>Take a screenshot of your game</li>
+            <li>Click back to the gallery folder</li>
+            <li>Open the img folder in gallery</li>
+            <li>Click "Add File"</li>
+            <li>Select "Upload File"</li>
+            <li>Name your image file the file name for your game</li>
+            <li>Click "Commit File"</li>
+          </ul>
+        </div>
+        <img
+          class="media"
+          src="https://user-images.githubusercontent.com/27078897/184410129-a2dbf444-ae7e-442b-b4bd-d3f21ede54cc.png"
+          alt="screenshot of pull request page"
+        />
+      </div>
+
+      <a href="/" class="bottom-cta"><button class="btn active">Check out the gallery &raquo;</button></a>
+    </div>
   </div>
-</div>
+</body>
 
 <style lang="scss">
   @import '../styles/_variables.scss';
   @import '../styles/_fonts.scss';
   @import '../styles/_shared.scss';
 
+  body {
+    background: $accent;
+    color: #16161e;
+  }
+
   code {
     display: block;
     font-size: 1.2rem;
     font-family: inherit;
     font-weight: 400;
-    border: 4px solid rgb(64, 58, 77);
+    border: 4px solid desaturate(darken($accent, 8%), 30%);
     padding: 20px 30px;
     width: 65%;
     margin: auto;
@@ -167,7 +174,7 @@
   }
 
   .inner {
-    width: 85%;
+    width: 75%;
     margin: 0 auto;
     padding-bottom: 5vh;
   }
@@ -176,19 +183,19 @@
     padding: 10vh 0;
     margin: 0;
     margin-bottom: 5vh;
-    background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('/pixelart_ruins_girl_trees.png'),
-      $base-background;
+    color: white;
+    background: url('/background.jpg'), $base-background;
     background-size: cover;
     background-attachment: fixed;
-    background-position: center center;
+    background-position: bottom 200px center;
   }
 
   .heading-inner {
-    width: 85%;
+    width: 75%;
     margin: auto;
 
     a {
-      color: rgb(255, 122, 122);
+      color: $accent;
       text-decoration: underline;
     }
   }
