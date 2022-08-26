6d3df6111ad3d8b3c4b3d38ddc58c20a64f3ea25
leomcelroy <leomcelroy@gmail.com>
2022-08-26 15:05:39 -0400

    added note of open-source projects we used

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -443,7 +443,7 @@
     <p>
       Sprig was developed by a team at Hack Club with assistance from Brian Silverman (who helped develop Scratch and
       the precursor to LEGO Mindstorms), Vadim Gerasimov (engineer at Google who helped create Tetris when he was 15),
-      and Quentin Bolseé (researcher at MIT and Vrije University).
+      and Quentin Bolseé (researcher at MIT and Vrije University). We're also grateful for amazing open-source projects that make this possible like <a href="https://kalumajs.org/">Kaluma</a>, <a href="https://jerryscript.net/">JerryScript</a>, <a href="https://github.com/WebReflection/uhtml">uhtml</a>, and <a href="https://codemirror.net/">CodeMirror</a>.
     </p>
     <p>
       Dozens of contributors from our community of teen hackers were instrumental to Sprig’s development as well.
