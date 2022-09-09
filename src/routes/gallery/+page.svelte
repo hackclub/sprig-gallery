5d7cec2681f5ae53fae195d0e0ecec1297ab6f80
Kognise <felix.mattick@gmail.com>
2022-09-09 12:11:13 -0400

    Hack to fix errors?

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -1,5 +1,6 @@
 <script>
   import Card from '../../components/Card.svelte';
+  import { browser } from '$app/environment';
   import { onMount } from 'svelte';
 
   let games = [];
@@ -9,10 +10,11 @@
   let gallery;
 
   onMount(async () => {
+    if (!browser) return;
+
     // Preloader observes for changes in .gallery-inner childlist
     let observer = new MutationObserver(() => {
       preloader.style.opacity = '0';
-
       setTimeout(() => {
         preloader.style.display = 'none';
       }, 500);
@@ -22,269 +24,268 @@
 
     /////////////////////////////////
     try {
-      games = await fetch('https://editor.sprig.hackclub.com/metadata.json').then((res) =>
-        res.json(),
-      ).catch((error) => {
-        console.log(error)
-      });
-    }
-    catch (e) {
-      console.log("Cowabunga! A spicy error:")
-      console.log(e)
+      games = await fetch('https://editor.sprig.hackclub.com/metadata.json')
+        .then((res) => res.json())
+        .catch((error) => {
+          console.log(error);
+        });
+    } catch (e) {
+      console.log('Cowabunga! A spicy error:');
+      console.log(e);
       games = [
         {
-          "filename": "sokoban",
-          "title": "sokoban",
-          "author": "leo mcelroy",
-          "img": "",
-          "tags": ["beginner", "hackable", "puzzle"]
+          filename: 'sokoban',
+          title: 'sokoban',
+          author: 'leo mcelroy',
+          img: '',
+          tags: ['beginner', 'hackable', 'puzzle'],
         },
         {
-          "filename": "pong",
-          "title": "pong",
-          "author": "neesh",
-          "img": "",
-          "tags": ["classic"]
+          filename: 'pong',
+          title: 'pong',
+          author: 'neesh',
+          img: '',
+          tags: ['classic'],
         },
         {
-          "filename": "penguin_slide",
-          "title": "penguin_slide",
-          "author": "veehz",
-          "img": "",
-          "tags": ["puzzle"]
+          filename: 'penguin_slide',
+          title: 'penguin_slide',
+          author: 'veehz',
+          img: '',
+          tags: ['puzzle'],
         },
         {
-          "filename": "laser_tag",
-          "title": "laser_tag",
-          "author": "leo mcelroy",
-          "img": "",
-          "tags": ["hackable", "puzzle"]
+          filename: 'laser_tag',
+          title: 'laser_tag',
+          author: 'leo mcelroy',
+          img: '',
+          tags: ['hackable', 'puzzle'],
         },
         {
-          "filename": "15_puzzle",
-          "title": "15_puzzle",
-          "author": "maggie liu",
-          "img": "",
-          "tags": ["advanced"]
+          filename: '15_puzzle',
+          title: '15_puzzle',
+          author: 'maggie liu',
+          img: '',
+          tags: ['advanced'],
         },
         {
-          "filename": "9_puzzle",
-          "title": "9_puzzle",
-          "author": "brian silverman",
-          "img": "",
-          "tags": []
+          filename: '9_puzzle',
+          title: '9_puzzle',
+          author: 'brian silverman',
+          img: '',
+          tags: [],
         },
         {
-          "filename": "kindless",
-          "title": "kindless",
-          "author": "Ishan",
-          "img": "",
-          "tags": ["advanced"]
+          filename: 'kindless',
+          title: 'kindless',
+          author: 'Ishan',
+          img: '',
+          tags: ['advanced'],
         },
         {
-          "filename": "color_patterns",
-          "title": "color_patterns",
-          "author": "brian silverman",
-          "img": "",
-          "tags": []
+          filename: 'color_patterns',
+          title: 'color_patterns',
+          author: 'brian silverman',
+          img: '',
+          tags: [],
         },
         {
-          "filename": "matcher",
-          "title": "matcher",
-          "author": "brian silverman",
-          "img": "",
-          "tags": []
+          filename: 'matcher',
+          title: 'matcher',
+          author: 'brian silverman',
+          img: '',
+          tags: [],
         },
         {
-          "filename": "shoot_green_blobs",
-          "title": "shoot_green_blobs",
-          "author": "otterly-otter",
-          "img": "",
-          "tags": []
+          filename: 'shoot_green_blobs',
+          title: 'shoot_green_blobs',
+          author: 'otterly-otter',
+          img: '',
+          tags: [],
         },
         {
-          "filename": "crappy_bird",
-          "title": "crappy_bird",
-          "author": "ced",
-          "img": "",
-          "tags": []
+          filename: 'crappy_bird',
+          title: 'crappy_bird',
+          author: 'ced',
+          img: '',
+          tags: [],
         },
         {
-          "filename": "friendship",
-          "title": "friendship",
-          "author": "nicky case",
-          "img": "",
-          "tags": ["beginner", "puzzle"]
+          filename: 'friendship',
+          title: 'friendship',
+          author: 'nicky case',
+          img: '',
+          tags: ['beginner', 'puzzle'],
         },
         {
-          "filename": "getting_started",
-          "title": "getting_started",
-          "author": "hack club",
-          "img": "",
-          "tags": ["beginner", "tutorial"]
+          filename: 'getting_started',
+          title: 'getting_started',
+          author: 'hack club',
+          img: '',
+          tags: ['beginner', 'tutorial'],
         },
         {
-          "filename": "maze",
-          "title": "maze",
-          "author": "leo mcelroy",
-          "img": "",
-          "tags": ["beginner", "hackable", "puzzle"]
+          filename: 'maze',
+          title: 'maze',
+          author: 'leo mcelroy',
+          img: '',
+          tags: ['beginner', 'hackable', 'puzzle'],
         },
         {
-          "filename": "pyre",
-          "title": "pyre",
-          "author": "ced",
-          "img": "",
-          "tags": ["puzzle"]
+          filename: 'pyre',
+          title: 'pyre',
+          author: 'ced',
+          img: '',
+          tags: ['puzzle'],
         },
         {
-          "filename": "raycasting",
-          "title": "raycasting",
-          "author": "henry bass",
-          "img": "",
-          "tags": ["advanced"]
+          filename: 'raycasting',
+          title: 'raycasting',
+          author: 'henry bass',
+          img: '',
+          tags: ['advanced'],
         },
         {
-          "filename": "tolls",
-          "title": "tolls",
-          "author": "leo mcelroy",
-          "img": "",
-          "tags": ["WIP"]
+          filename: 'tolls',
+          title: 'tolls',
+          author: 'leo mcelroy',
+          img: '',
+          tags: ['WIP'],
         },
         {
-          "filename": "fogged",
-          "title": "fogged",
-          "author": "Jay Reddy",
-          "img": "",
-          "tags": ["WIP"]
+          filename: 'fogged',
+          title: 'fogged',
+          author: 'Jay Reddy',
+          img: '',
+          tags: ['WIP'],
         },
         {
-          "filename": "multiplayer_soccer",
-          "title": "multiplayer_soccer",
-          "author": "Shannon Yeow",
-          "img": "",
-          "tags": ["multiplayer"]
+          filename: 'multiplayer_soccer',
+          title: 'multiplayer_soccer',
+          author: 'Shannon Yeow',
+          img: '',
+          tags: ['multiplayer'],
         },
         {
-          "filename": "snakey_snake",
-          "title": "snakey_snake",
-          "author": "Alexander Chin",
-          "img": "",
-          "tags": ["WIP", "puzzle"]
+          filename: 'snakey_snake',
+          title: 'snakey_snake',
+          author: 'Alexander Chin',
+          img: '',
+          tags: ['WIP', 'puzzle'],
         },
         {
-          "filename": "platform_rogue",
-          "title": "platform_rogue",
-          "author": "farreltobias",
-          "img": "",
-          "tags": ["advanced"]
+          filename: 'platform_rogue',
+          title: 'platform_rogue',
+          author: 'farreltobias',
+          img: '',
+          tags: ['advanced'],
         },
         {
-          "filename": "among_us_maze",
-          "title": "among_us_maze",
-          "author": "Riya and Christy",
-          "img": "",
-          "tags": ["advanced", "web-only"]
+          filename: 'among_us_maze',
+          title: 'among_us_maze',
+          author: 'Riya and Christy',
+          img: '',
+          tags: ['advanced', 'web-only'],
         },
         {
-          "filename": "256",
-          "title": "256",
-          "author": "Jacky Zhao",
-          "img": "",
-          "tags": ["classic"]
+          filename: '256',
+          title: '256',
+          author: 'Jacky Zhao',
+          img: '',
+          tags: ['classic'],
         },
         {
-          "filename": "minesweeper",
-          "title": "minesweeper",
-          "author": "sam liu",
-          "img": "",
-          "tags": ["classic"]
+          filename: 'minesweeper',
+          title: 'minesweeper',
+          author: 'sam liu',
+          img: '',
+          tags: ['classic'],
         },
         {
-          "filename": "connect_four",
-          "title": "connect_four",
-          "author": "sam liu",
-          "img": "",
-          "tags": ["classic"]
+          filename: 'connect_four',
+          title: 'connect_four',
+          author: 'sam liu',
+          img: '',
+          tags: ['classic'],
         },
         {
-          "filename": "SprazeJS",
-          "title": "SprazeJS",
-          "author": "Peipr",
-          "img": "",
-          "tags": []
+          filename: 'SprazeJS',
+          title: 'SprazeJS',
+          author: 'Peipr',
+          img: '',
+          tags: [],
         },
         {
-          "filename": "nomis",
-          "title": "nomis",
-          "author": "Cheru Berhanu",
-          "img": "",
-          "tags": ["classic", "hackable"]
+          filename: 'nomis',
+          title: 'nomis',
+          author: 'Cheru Berhanu',
+          img: '',
+          tags: ['classic', 'hackable'],
         },
         {
-          "filename": "snek",
-          "title": "snek",
-          "author": "gammacarrot",
-          "img": "",
-          "tags": ["classic"]
+          filename: 'snek',
+          title: 'snek',
+          author: 'gammacarrot',
+          img: '',
+          tags: ['classic'],
         },
         {
-          "filename": "sokoban_plus",
-          "title": "sokoban_plus",
-          "author": "Leonard (Omay)",
-          "img": "",
-          "tags": ["puzzle"]
+          filename: 'sokoban_plus',
+          title: 'sokoban_plus',
+          author: 'Leonard (Omay)',
+          img: '',
+          tags: ['puzzle'],
         },
         {
-          "filename": "tictactoe",
-          "title": "tictactoe",
-          "author": "reesericci",
-          "img": "",
-          "tags": ["classic", "multiplayer"]
+          filename: 'tictactoe',
+          title: 'tictactoe',
+          author: 'reesericci',
+          img: '',
+          tags: ['classic', 'multiplayer'],
         },
         {
-          "filename": "alien",
-          "title": "alien",
-          "author": "anshimathur0325",
-          "img": "",
-          "tags": []
+          filename: 'alien',
+          title: 'alien',
+          author: 'anshimathur0325',
+          img: '',
+          tags: [],
         },
         {
-          "filename": "mistery_of_scooter",
-          "title": "mistery_of_scooter",
-          "author": "LucasHT22",
-          "img": "",
-          "tags": ["WIP"]
+          filename: 'mistery_of_scooter',
+          title: 'mistery_of_scooter',
+          author: 'LucasHT22',
+          img: '',
+          tags: ['WIP'],
         },
         {
-          "filename": "balloon",
-          "title": "balloon",
-          "author": "LucasHT22",
-          "img": "",
-          "tags": ["WIP"]
+          filename: 'balloon',
+          title: 'balloon',
+          author: 'LucasHT22',
+          img: '',
+          tags: ['WIP'],
         },
         {
-          "filename": "purge",
-          "title": "purge",
-          "author": "noviicee",
-          "img": "",
-          "tags": ["puzzle"]
+          filename: 'purge',
+          title: 'purge',
+          author: 'noviicee',
+          img: '',
+          tags: ['puzzle'],
         },
         {
-          "filename": "confusing_conditions",
-          "title": "confusing_conditions",
-          "author": "Sooraj",
-          "img": "",
-          "tags": []
+          filename: 'confusing_conditions',
+          title: 'confusing_conditions',
+          author: 'Sooraj',
+          img: '',
+          tags: [],
         },
         {
-          "filename": "Virtual_Machine",
-          "title": "Virtual_Machine",
-          "author": "N_Rizwan",
-          "img": "",
-          "tags": []
-        }
-      ]
+          filename: 'Virtual_Machine',
+          title: 'Virtual_Machine',
+          author: 'N_Rizwan',
+          img: '',
+          tags: [],
+        },
+      ];
     }
     tags = [...new Set(games.reduce((p, c) => [...p, ...c.tags], []))];
   });
