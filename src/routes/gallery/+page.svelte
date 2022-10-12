3d6a6e8c90979e063f12ca2a61d7bf04e1f2a1a1
Yoda <yoda@yodacode.xyz>
2022-10-12 15:01:50 -0700

    Add search by author

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -125,7 +125,7 @@
 
       {#each games as game}
         <!-- Tutorials first, or whatever the filter is -->
-        {#if (activeFilter === '_new' && game.isNew) || (game.tags.includes(activeFilter || 'tutorial') && searchQuery === "") || (game.title.toLowerCase().indexOf(searchQuery.toLowerCase()) !== -1 && searchQuery !== "")}
+        {#if (activeFilter === '_new' && game.isNew) || (game.tags.includes(activeFilter || 'tutorial') && searchQuery === "") || ((game.title.toLowerCase().indexOf(searchQuery.toLowerCase()) !== -1 || game.author.toLowerCase().indexOf(searchQuery.toLowerCase()) !== -1) && searchQuery !== "")}
           <Card isNew={game.isNew} title={game.title} tags={game.tags} author={game.author} filename={game.filename} />
         {/if}
       {/each}
