ba1633583766ceab2d22f8358283c34fac53a7f7
Omay <70179858+Omay238@users.noreply.github.com>
2022-11-07 18:15:35 -0700

    test server

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -22,7 +22,7 @@
     });
     observer.observe(gallery, { childList: true });
 
-    games = await fetch('https://editor.sprig.hackclub.com/metadata.json').then((res) => res.json());
+    games = await fetch('https://sprig-git-titlefilenamefix.hackclub.dev/metadata.json').then((res) => res.json());
     [...games]
       .sort((a, b) => new Date(b.addedOn) - new Date(a.addedOn))
       .slice(0, 9)
